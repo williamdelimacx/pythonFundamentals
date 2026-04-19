b1a0bc16f0135140a3b37d419bf575fdfa0a6e29
William de Lima <williamdelimacx@gmail.com>
2026-04-19 18:11:54 -0300

    feat: numbers

---
+++ variable_types/numbers.py
@@ -0,0 +1,38 @@
+# Integer
+my_int = 10
+print("Integer = ", my_int)
+
+# Float
+my_float = 10.5
+print("Float = ", my_float)
+
+# type() function to check the type of variable
+print("Type of integer: ", type(my_int))
+print("Type of float: ", type(my_float))
+
+# Sum +
+sum_result = my_int + my_float
+print("Sum of integer and float: ", sum_result)
+
+# Subtraction -
+sub_result = my_int - my_float
+print("Subtraction of integer and float: ", sub_result)
+
+# Multiplication *
+mul_result = my_int * my_float
+print("Multiplication of integer and float: ", mul_result)
+
+# Division /
+div_result = my_int / my_float
+print("Division of integer and float: ", div_result)
+
+# Integer division //
+int_div_result = my_int // 3
+print("Integer division of integer by 3: ", int_div_result)
+
+print("Integer value = ", int(my_float))  # Convert float to integer
+print("Float value = ", float(my_int))  # Convert integer to float
+
+# Modulo %
+mod_result = my_int % 3
+print("Modulo of integer by 3: ", mod_result)
