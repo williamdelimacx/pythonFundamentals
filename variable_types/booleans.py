9c619c7bd94fdb489249176a1bfb17c373e13c69
William de Lima <williamdelimacx@gmail.com>
2026-04-21 13:57:22 -0300

    feat: booleans

---
+++ variable_types/booleans.py
@@ -0,0 +1,30 @@
+# True condition
+True
+
+# False condition
+False
+
+# Conditional statement using boolean values
+if True:
+    print("This condition is true.")
+else:
+    print("This condition is false.")
+
+# Logical operations
+if True and True:
+    print("Both conditions are true.") # This will execute
+
+if True and False:
+    print("One condition is false.") # This will not execute
+
+if False and False:
+    print("Both conditions are false.") # This will not execute
+
+if True or False:
+    print("At least one condition is true.") # This will execute
+
+if False or False:
+    print("Both conditions are false.") # This will not execute
+
+if True or True:
+    print("At least one condition is true.") # This will execute
