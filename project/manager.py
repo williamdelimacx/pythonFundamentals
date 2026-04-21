8e0fa39efb4ca60afc78ea42e2cf72e586992259
William de Lima <williamdelimacx@gmail.com>
2026-04-21 16:13:08 -0300

    feat: loop while

---
+++ project/manager.py
@@ -0,0 +1,14 @@
+while True:
+  print("\n Task Manager Menu:")
+  print("1. Add Task")
+  print("2. View Tasks")
+  print("3. Edit Task")
+  print("4. Mark Task as Complete")
+  print("5. Delete Task")
+  print("6. Exit")
+
+  choice = input("Enter your choice (1-6): ")
+
+  if choice == '6':
+    print("Exiting Task Manager. Goodbye!")
+  break
---
+++ while.py
@@ -0,0 +1,14 @@
+# Example of using while loop to print numbers from 1 to 5
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+
+# Example of using while loop with break statement
+j = 1
+while j <= 5:
+    if j == 3:
+        break
+    print(j)
+    j += 1
+    
