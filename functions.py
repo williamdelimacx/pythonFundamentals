9cb0b1c0f6d53c39fc31c0904263e9c5affa4d5b
William de Lima <williamdelimacx@gmail.com>
2026-04-21 16:26:57 -0300

    feat: functions

---
+++ functions.py
@@ -0,0 +1,18 @@
+# Example of a function that says hello to a name
+def say_hello(name):
+    print (f"Hello, {name}!")
+
+say_hello("Alice")  # This will print "Hello, Alice!"
+say_hello("Bob")    # This will print "Hello, Bob!"
+
+# Example of a function receive a number and return its square
+def square(number):
+    return number ** 2
+result = square(5)  # This will return 25
+print(result)       # This will print 25
+
+# Example of a function that takes two numbers and returns their sum
+def sum(a, b):
+    return a + b
+sum_result = sum(3, 4)  # This will return 7
+print(sum_result)        # This will print 7
